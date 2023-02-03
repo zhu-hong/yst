bf4ae6c7110fdd3dbf32048fe731bf08a2b07a20
zhuh <zhuh@wingoht.com>
2023-02-03 14:43:19 +0800

    svg loader

--- src/utils/svg.js
+++ src/utils/svg.js
@@ -2,6 +2,6 @@ import Vue from 'vue'
 import Icon from '@/components/Icon.vue'
 
 const requireAll = (requireContext) => requireContext.keys().map(requireContext)
-requireAll(require.context('./assets/icons', false, /\.svg$/))
+requireAll(require.context('../assets/icons', false, /\.svg$/))
 
 Vue.component('Icon', Icon)
