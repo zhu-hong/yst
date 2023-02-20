0551f065743fc2a1521ded3d2a247835c3525824
zhuh <zhuh@wingoht.com>
2023-02-20 15:15:27 +0800

    components

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <script setup>
-import Header from './components/Header.vue'
+import CHeader from './components/CHeader.vue'
 import { RouterView } from 'vue-router'
 </script>
 
 <template>
   <div class="w-screen h-screen flex flex-col overflow-hidden">
-    <Header/>
+    <CHeader/>
     <RouterView class="flex-auto overflow-hidden"></RouterView>
   </div>
 </template>
--- src/components/CDrawer.vue
+++ src/components/CDrawer.vue
@@ -10,13 +10,14 @@ const props = defineProps({
     default: '标题',
   },
 })
-const emits = defineEmits(['confirm'])
+const emits = defineEmits(['confirm', 'close'])
 const visible = ref(false)
 const open = () => {
   visible.value = true
 }
 const close = () => {
   visible.value = false
+  emits('close')
 }
 const onModalClick = () => {
   if(!props.mc) return
@@ -32,9 +33,9 @@ defineExpose({
 </script>
 
 <template>
-  <transition name="drawer">
-    <div @click.self="onModalClick" class="drawer-container" v-if="visible">
-      <div class="drawer-wrapper">
+  <transition name="cdrawer">
+    <div @click.self="onModalClick" class="cdrawer-container" v-if="visible">
+      <div class="cdrawer-wrapper">
         <div class="h-64px flex-none border-b border-normal px-24px flex justify-between items-center">
           <span class="text-base font-medium">{{ title }}</span>
           <svg @click="close" class="cursor-pointer" width="20" height="20" xmlns="http://www.w3.org/2000/svg"><path d="M17.071 2.929a.833.833 0 0 1 0 1.178L11.18 10l5.892 5.893a.833.833 0 0 1-1.178 1.178L10 11.18 4.107 17.07a.833.833 0 0 1-1.178-1.178l5.893-5.894L2.93 4.107A.833.833 0 1 1 4.107 2.93l5.894 5.892 5.892-5.892a.833.833 0 0 1 1.178 0Z" fill="#646A73" fill-rule="evenodd"/></svg>
@@ -52,14 +53,14 @@ defineExpose({
 </template>
 
 <style lang="postcss">
-.drawer-enter-active, .drawer-leave-active {
+.cdrawer-enter-active, .cdrawer-leave-active {
   transition: all .5s ease;
 }
-.drawer-enter, .drawer-leave-to {
+.cdrawer-enter, .cdrawer-leave-to {
   transform: translateX(420px);
   opacity: 0;
 }
-.drawer {
+.cdrawer {
   &-container {
     position: fixed;
     width: calc(100vw + 420px);
@@ -77,7 +78,7 @@ defineExpose({
     top: 0;
     right: 0;
     height: 100%;
-    width: 480px;
+    width: 420px;
     box-shadow: -4px 0px 13px 0px rgba(0,0,0,0.13);
     background-color: #ffffff;
   }
